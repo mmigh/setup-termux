fce6361ea3f7aa03bfd8338ba4d486bd92b8847e
Primal_han <hieu30671@gmail.com>
2025-06-07 16:03:15 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,17 +8,16 @@ fi
 # Thiết lập quyền truy cập bộ nhớ
 termux-setup-storage
 
-# Cập nhật gói và thêm root/x11 repo để tránh lỗi mirror
+# Cập nhật gói và thêm repo tránh lỗi mirror
 yes | pkg update
 yes | pkg install root-repo x11-repo -y
 
-# Tự động đổi repo thông qua script GitHub
+# Đổi repo Termux (dùng link mới)
 . <(curl -fsSL https://raw.githubusercontent.com/mmigh/setup-termux/refs/heads/main/termux-change-repo.sh)
 
-# Nâng cấp gói và cài đặt công cụ cần thiết
+# Nâng cấp gói và cài công cụ cần thiết
 yes | pkg upgrade
 yes | pkg install python android-tools python-pip -y
 
-# Cài đặt các thư viện Python bạn cần
-pip install --upgrade pip
+# Cài thư viện Python cần thiết (không nâng pip để tránh lỗi Termux)
 pip install requests psutil prettytable pytz pyjwt pycryptodome rich colorama flask discord python-socketio
