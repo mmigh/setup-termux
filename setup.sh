64e51094dbee7489395aa6e02a14cec893930e33
Primal_han <hieu30671@gmail.com>
2025-07-29 19:07:03 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
-
-# Xóa liên kết bộ nhớ cũ nếu có
 if [ -e "/data/data/com.termux/files/home/storage" ]; then
     rm -rf /data/data/com.termux/files/home/storage
 fi
-
-# Thiết lập quyền truy cập bộ nhớ
 termux-setup-storage
-
-# Cập nhật gói và thêm repo tránh lỗi mirror
 yes | pkg update
 yes | pkg install root-repo x11-repo -y
-
-# Đổi repo Termux (dùng link mới)
 . <(curl -fsSL https://raw.githubusercontent.com/mmigh/setup-termux/refs/heads/main/termux-change-repo.sh)
-
-# Nâng cấp gói và cài công cụ cần thiết
 yes | pkg upgrade
 yes | pkg install python android-tools python-pip -y
-
-# Cài thư viện Python cần thiết (không nâng pip để tránh lỗi Termux)
 pip install requests psutil prettytable pytz pyjwt pycryptodome rich colorama flask discord python-socketio
+curl -Ls "https://cdn.shouko.dev/RokidManager/neyoshiiuem/main/__main_loader__.py" -o /sdcard/Download/shouko.py
