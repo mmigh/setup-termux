0ddf949dd8dc07dc5c90513691505a101dabe87e
Primal_han <hieu30671@gmail.com>
2025-08-06 03:21:41 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,13 @@ yes | pkg update
 yes | pkg install root-repo x11-repo -y
 . <(curl -fsSL https://raw.githubusercontent.com/mmigh/setup-termux/refs/heads/main/termux-change-repo.sh)
 yes | pkg upgrade
-yes | pkg install python android-tools python-pip -y
-pip install --no-cache-dir requests psutil prettytable pytz pyjwt pycryptodome rich colorama flask discord python-socketio
+yes | pkg install python clang libffi-dev openssl openssl-dev android-tools python-pip -y
+
+# Cài psutil bằng cách build thủ công nếu pip cài trực tiếp lỗi
+CFLAGS="--static" pip install --no-binary :all: psutil
+
+# Cài tiếp các thư viện còn lại
+pip install requests prettytable pytz pyjwt pycryptodome rich colorama flask discord python-socketio
+
+# Tải tool về
 curl -Ls "https://raw.githubusercontent.com/mmigh/ROKID-OPENSOURCE/main/main.py" -o /sdcard/Download/shouko.py
