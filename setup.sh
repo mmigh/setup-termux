769b61b2d5dbd3bb66788388ab41d5a0947ce840
Primal_han <hieu30671@gmail.com>
2025-08-06 12:08:45 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,15 @@
 if [ -e "/data/data/com.termux/files/home/storage" ]; then
     rm -rf /data/data/com.termux/files/home/storage
 fi
+
 termux-setup-storage
 yes | pkg update
 yes | pkg install root-repo x11-repo -y
 . <(curl -fsSL https://raw.githubusercontent.com/mmigh/setup-termux/refs/heads/main/termux-change-repo.sh)
 yes | pkg upgrade
-yes | pkg install python clang libffi-dev openssl openssl-dev android-tools python-pip -y
-
-# Cài psutil bằng cách build thủ công nếu pip cài trực tiếp lỗi
-CFLAGS="--static" pip install --no-binary :all: psutil
-
-# Cài tiếp các thư viện còn lại
+yes | pkg install python android-tools python-pip tsu libexpat git openssl lua53 imagemagick chafa -y
 pip install requests prettytable pytz pyjwt pycryptodome rich colorama flask discord python-socketio
 
-# Tải tool về
+CFLAGS="-Wno-error=implicit-function-declaration" pip install psutil
+
 curl -Ls "https://raw.githubusercontent.com/mmigh/ROKID-OPENSOURCE/main/main.py" -o /sdcard/Download/shouko.py
